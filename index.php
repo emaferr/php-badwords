1618223ab4dea7f2e727102753b8267d8349eb43
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-21 17:16:43 +0200

    Add a word censored print on the screen how many and which words have been censored

--- index.php
+++ index.php
@@ -4,70 +4,74 @@
 // var_dump($_GET);
 
 $censored_word = $_GET["censored_word"];
+$censored_word_2 = $_GET["censored_word_2"];
+$censored_words = [$censored_word,$censored_word_2];
 
 // php code
-$lyrics =  'Loro non sanno di che parlo
-            Voi siete sporchi fra’ di fango
-            Giallo di siga’ fra le dita
-            Io con la siga’ camminando
-            Scusami ma ci credo tanto
-            Che posso fare questo salto
-            Anche se la strada è in salita
-            Per questo ora mi sto allenando
-            E buonasera signore e signori
+$title_song = 'Fuori di Testa';
 
-            Fuori gli attori
-            Vi conviene toccarvi i coglioni
-            Vi conviene stare zitti e buoni
-            Qui la gente è strana tipo spacciatori
-            Troppe notti stavo chiuso fuori
-            Mo’ li prendo a calci ‘sti portoni
-            Sguardo in alto tipo scalatori
-            Quindi scusa mamma se sto sempre fuori, ma
-            Sono fuori di testa ma diverso da loro
-            E tu sei fuori di testa ma diversa da loro
-            Siamo fuori di testa ma diversi da loro
-            Siamo fuori di testa ma diversi da loro
-
-            Io
-            Ho scritto pagine e pagine
-            Ho visto sale poi lacrime
-            Questi uomini in macchina
-            Non scalare le rapide
-            Scritto sopra una lapide
-            In casa mia non c’è Dio
-            Ma se trovi il senso del tempo
-            Risalirai dal tuo oblio
-            E non c’è vento che fermi
-            La naturale potenza
-            Dal punto giusto di vista
-            Del vento senti l’ebrezza
-            Con ali in cera alla schiena
-            Ricercherò quell’altezza
-            Se vuoi fermarmi ritenta
-            Prova a tagliarmi la testa
-            Perché
-            Sono fuori di testa ma diverso da loro
-            E tu sei fuori di testa ma diversa da loro
-            Siamo fuori di testa ma diversi da loro
-            Siamo fuori di testa ma diversi da loro
-            Parla la gente purtroppo
-            Parla non sa di che cazzo parla
-            Tu portami dove sto a galla
-            Che qui mi manca l’aria
-
-            Parla la gente purtroppo
-            Parla non sa di che cazzo parla
-            Tu portami dove sto a galla
-            Che qui mi manca l’aria
-            Parla la gente purtroppo
-            Parla non sa di che cazzo parla
-            Tu portami dove sto a galla
-            Che qui mi manca l’aria
-            Ma sono fuori di testa ma diverso da loro
-            E tu sei fuori di testa ma diversa da loro
-            Siamo fuori di testa ma diversi da loro
-            Siamo fuori di testa ma diversi da loro
+$lyrics =  'Loro non sanno di che parlo<br/>
+            Voi siete sporchi fra’ di fango<br/>
+            Giallo di siga’ fra le dita<br/>
+            Io con la siga’ camminando<br/>
+            Scusami ma ci credo tanto<br/>
+            Che posso fare questo salto<br/>
+            Anche se la strada è in salita<br/>
+            Per questo ora mi sto allenando<br/>
+            E buonasera signore e signori<br/>
+            <br/>
+            Fuori gli attori<br/>
+            Vi conviene toccarvi i coglioni<br/>
+            Vi conviene stare zitti e buoni<br/>
+            Qui la gente è strana tipo spacciatori<br/>
+            Troppe notti stavo chiuso fuori<br/>
+            Mo’ li prendo a calci ‘sti portoni<br/>
+            Sguardo in alto tipo scalatori<br/>
+            Quindi scusa mamma se sto sempre fuori, ma<br/>
+            Sono fuori di testa ma diverso da loro<br/>
+            E tu sei fuori di testa ma diversa da loro<br/>
+            Siamo fuori di testa ma diversi da loro<br/>
+            Siamo fuori di testa ma diversi da loro<br/>
+            <br/>
+            Io<br/>
+            Ho scritto pagine e pagine<br/>
+            Ho visto sale poi lacrime<br/>
+            Questi uomini in macchina<br/>
+            Non scalare le rapide<br/>
+            Scritto sopra una lapide<br/>
+            In casa mia non c’è Dio<br/>
+            Ma se trovi il senso del tempo<br/>
+            Risalirai dal tuo oblio<br/>
+            E non c’è vento che fermi<br/>
+            La naturale potenza<br/>
+            Dal punto giusto di vista<br/>
+            Del vento senti l’ebrezza<br/>
+            Con ali in cera alla schiena<br/>
+            Ricercherò quell’altezza<br/>
+            Se vuoi fermarmi ritenta<br/>
+            Prova a tagliarmi la testa<br/>
+            Perché<br/>
+            Sono fuori di testa ma diverso da loro<br/>
+            E tu sei fuori di testa ma diversa da loro<br/>
+            Siamo fuori di testa ma diversi da loro<br/>
+            Siamo fuori di testa ma diversi da loro<br/>
+            Parla la gente purtroppo<br/>
+            Parla non sa di che cazzo parla<br/>
+            Tu portami dove sto a galla<br/>
+            Che qui mi manca l’aria<br/>
+            <br/>
+            Parla la gente purtroppo<br/>
+            Parla non sa di che cazzo parla<br/>
+            Tu portami dove sto a galla<br/>
+            Che qui mi manca l’aria<br/>
+            Parla la gente purtroppo<br/>
+            Parla non sa di che cazzo parla<br/>
+            Tu portami dove sto a galla<br/>
+            Che qui mi manca l’aria<br/>
+            Ma sono fuori di testa ma diverso da loro<br/>
+            E tu sei fuori di testa ma diversa da loro<br/>
+            Siamo fuori di testa ma diversi da loro<br/>
+            Siamo fuori di testa ma diversi da loro<br/>
             Noi siamo diversi da loro';
 
 $number_of_words_text = count(explode(' ', $lyrics));                
@@ -91,24 +95,27 @@ $number_of_words_text = count(explode(' ', $lyrics));
 
     <h1>The lyrics of the songs</h1>
 
+    <h2><?php echo $title_song;?></h2>
+
     <p>
         <?php echo $lyrics;?>
     </p>
 
-    <h2>Number of words in the text: <?php echo $number_of_words_text;?></h2>
+    <h3>Number of words in the text: <?php echo $number_of_words_text;?></h3>
 
-    <h2>Number of characters in the text: <?php echo mb_strlen($lyrics);?></h2>
+    <h3>Number of characters in the text: <?php echo mb_strlen($lyrics);?></h3>
 
     <hr>
     
     <h1>Song lyrics censored</h1>
 
-    <!-- http://localhost:8888/php/php-badwords/?censored_word=cazzo -->
+    <!-- http://localhost:8888/php/php-badwords/?censored_word=cazzo&censored_word_2=coglioni -->
     <p>
-        <?php echo str_replace($censored_word, '***', $lyrics);?>
+        <?php echo str_replace(array($censored_word,$censored_word_2), '<strong style="color : red;">***</strong>', $lyrics);?>
     </p>
 
-    <h4>Censored word: <?php echo $censored_word;?> </h4>
+    <h3>Number of censored words in the text: <?php echo count($censored_words);?></h3>
+    <h3>Censored words: <?php echo $censored_words[0]." ".$censored_words[1];?> </h3>
 
 </body>
 
